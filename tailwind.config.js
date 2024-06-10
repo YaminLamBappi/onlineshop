77f46fb30c369f13b0e027f35f03fbf2672c6d2d
Yamin Lam Bappi <yaminlambappi@gmail.com>
2024-06-10 16:25:03 +0600

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.css",
+    "./static/**/*.js",
+    // Add other paths as necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
