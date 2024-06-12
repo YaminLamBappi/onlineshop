1a87906c8880c3d656b8c05c016b54b37a05334f
Yamin Lam Bappi <yaminlambappi@gmail.com>
2024-06-12 20:28:37 +0600

    favicon added

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 module.exports = {
+  content: ["./templates/**/*.html", "./static/src/js/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#1a202c",
+        light: "#ffffff",
+      },
+    },
+  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
